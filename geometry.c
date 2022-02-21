164cfc629cae43b1f698601743f53b598e414da4
Valeria Granina <v_granina.04@mail.ru>
2022-02-21 07:47:38 +0200

    izm

--- geometry.c
+++ geometry.c
@@ -10,21 +10,21 @@ int main ()
     switch(n)
     {
         case 1:
-        printf ("Vedite radius:\n");
-        scanf ("%d",&r);
-        perimetr = 3.14*2*r;
-        s = 3.14*r*r;
-        printf ("\nperimetr = %.2f   s = %.2f\n",perimetr, s);
-        break;
+            printf ("Vedite radius:\n");
+            scanf ("%d",&r);
+            perimetr = 3.14*2*r;
+            s = 3.14*r*r;
+            printf ("\nperimetr = %.2f   s = %.2f\n",perimetr, s);
+            break;
         case 2:
-        printf ("Vedite storoni:");
-        scanf ("%d %d %d",&a, &b, &c);
-        perimetr = (a+b+c);
-        s = sqrt(perimetr/2*(perimetr/2-a)*(perimetr/2-b)*(perimetr/2-c));
-        printf ("\nperimetr = %.2f   s = %.2f\n",perimetr, s);
-        break;
+            printf ("Vedite storoni:");
+            scanf ("%d %d %d",&a, &b, &c);
+            perimetr = (a+b+c);
+            s = sqrt(perimetr/2*(perimetr/2-a)*(perimetr/2-b)*(perimetr/2-c));
+            printf ("\nperimetr = %.2f   s = %.2f\n",perimetr, s);
+            break;
         default:
-        printf("\nERROR\n");
+            printf("\nERROR\n");
 
     }
     return 0;
