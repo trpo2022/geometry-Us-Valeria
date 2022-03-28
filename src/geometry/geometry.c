8d214b96a462800832d5aa895ca27d5e224a1ae9
Valeria Granina <v_granina.04@mail.ru>
2022-03-28 08:08:58 +0200

    Src

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -1,21 +1,26 @@
-#include "libgeo/circle.h"
-#include "libgeo/triangle.h"
 #include <math.h>
 #include <stdio.h>
+#include "libgeo/check.c"
 
 int main()
 {
-    int a;
-    printf("1. Cirle\n2. Triangle\nChoose your figure: ");
-    scanf(" %d", &a);
-    if (a == 1) {
-        circle();
+    float x0, y0, r;
+    float x1, y1, x2, y2, x3, y3;
+    printf ("circle: x0, y0, r ");
+    scanf ("%f %f %f", &x0,&y0,&r);
+    printf ("treug: x1, y1 ");
+    scanf ("%f %f ", &x1,&y1);
+    printf ("treug: x2, y2 ");
+    scanf ("%f %f ", &x2,&y2);
+    printf ("treug: x3, y3 ");
+    scanf ("%f %f", &x3,&y3);
+    if ((r>0)&&(x1!=x2)&&(x2!=x3)&&(x1!=x3)&&(y1!=y2)&&(y2!=y3)&&(y1!=y3)) 
+    {
+        check(x0, y0, r, x1, y1, x2, y2, x3, y3);
     }
-    if (a == 2) {
-        triangle();
-    } else {
-        printf("Error\n");
-        return 0;
+    else
+    {
+        printf ("No");
     }
     return 0;
 }
---
+++ src/libgeo/check.c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <stdio.h>
+
+
+void check(float x0, float y0, float r, float x1, float y1, float x2, float y2, float x3, float y3)
+{
+    if (((sqrt((x0-x1)*(x0-x1)+(y0-y1)*(y0-y1))>r) && (sqrt((x0-x2)*(x0-x2)+(y0-y2)*(y0-y2))>r) && (sqrt((x0-x3)*(x0-x3)+(y0-y3)*(y0-y3))>r)) || ((sqrt((x0-x1)*(x0-x1)+(y0-y1)*(y0-y1))<r) && (sqrt((x0-x2)*(x0-x2)+(y0-y2)*(y0-y2))<r) && (sqrt((x0-x3)*(x0-x3)+(y0-y3)*(y0-y3))<r)))
+    {
+        printf ("don't overlap");
+    }
+    else
+    {
+        printf ("overlap");
+    }
+}
