169d649b48ed9ff4ac2a7af838a935c488baba07
Valeria Granina <v_granina.04@mail.ru>
2022-03-28 08:24:55 +0200

    fix src

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -4,23 +4,23 @@
 
 int main()
 {
-    float x0, y0, r;
-    float x1, y1, x2, y2, x3, y3;
-    printf ("circle: x0, y0, r ");
-    scanf ("%f %f %f", &x0,&y0,&r);
-    printf ("treug: x1, y1 ");
-    scanf ("%f %f ", &x1,&y1);
-    printf ("treug: x2, y2 ");
-    scanf ("%f %f ", &x2,&y2);
-    printf ("treug: x3, y3 ");
-    scanf ("%f %f", &x3,&y3);
-    if ((r>0)&&(x1!=x2)&&(x2!=x3)&&(x1!=x3)&&(y1!=y2)&&(y2!=y3)&&(y1!=y3)) 
-    {
-        check(x0, y0, r, x1, y1, x2, y2, x3, y3);
-    }
-    else
-    {
-        printf ("No");
-    }
-    return 0;
+	float x0, y0, r;
+	float x1, y1, x2, y2, x3, y3;
+	printf ("circle: x0, y0, r ");
+	scanf ("%f %f %f", &x0,&y0,&r);
+	printf ("treug: x1, y1 ");
+	scanf ("%f %f ", &x1,&y1);
+	printf ("treug: x2, y2 ");
+	scanf ("%f %f ", &x2,&y2);
+	printf ("treug: x3, y3 ");
+	scanf ("%f %f", &x3,&y3);
+	if ((r>0)&&(x1!=x2)&&(x2!=x3)&&(x1!=x3)&&(y1!=y2)&&(y2!=y3)&&(y1!=y3)) 
+	{
+		check(x0, y0, r, x1, y1, x2, y2, x3, y3);
+	}
+	else
+	{
+		printf ("No");
+	}
+	return 0;
 }
